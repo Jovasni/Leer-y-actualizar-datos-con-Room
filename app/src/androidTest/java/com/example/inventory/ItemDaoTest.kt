3c2535c6cd6bd93b5865d41ff8e3c1dd5cc76402
JovaniZG <s22120168@alumnos.itsur.edu.mx>
2025-10-27 00:23:36 -0600

    Se implemento la ventana de Item details al igual que se agregaron los productos disponibles en la base de datos

--- app/src/androidTest/java/com/example/inventory/ItemDaoTest.kt
+++ app/src/androidTest/java/com/example/inventory/ItemDaoTest.kt
@@ -39,7 +39,7 @@ class ItemDaoTest {
     private lateinit var itemDao: ItemDao
     private lateinit var inventoryDatabase: InventoryDatabase
     private val item1 = Item(1, "Manzanas", 17.0, 20)
-    private val item2 = Item(2, "Platanos", 15.0, 97)
+    private val item2 = Item(2, "Platanos", 20.0, 97)
 
     @Before
     fun createDb() {
