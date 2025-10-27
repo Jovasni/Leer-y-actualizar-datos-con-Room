58550ec7991beb9811bf3aff70c836bc624b2928
JovaniZG <s22120168@alumnos.itsur.edu.mx>
2025-10-27 00:20:12 -0600

    Se implemento la ventana de Item details al igual que se agregaron los productos disponibles en la base de datos y se

--- app/src/androidTest/java/com/example/inventory/ItemDaoTest.kt
+++ app/src/androidTest/java/com/example/inventory/ItemDaoTest.kt
@@ -38,14 +38,13 @@ class ItemDaoTest {
 
     private lateinit var itemDao: ItemDao
     private lateinit var inventoryDatabase: InventoryDatabase
-    private val item1 = Item(1, "Apples", 10.0, 20)
-    private val item2 = Item(2, "Bananas", 15.0, 97)
+    private val item1 = Item(1, "Manzanas", 10.0, 20)
+    private val item2 = Item(2, "Platanos", 15.0, 97)
 
     @Before
     fun createDb() {
         val context: Context = ApplicationProvider.getApplicationContext()
-        // Using an in-memory database because the information stored here disappears when the
-        // process is killed.
+
         inventoryDatabase = Room.inMemoryDatabaseBuilder(context, InventoryDatabase::class.java)
             // Allowing main thread queries, just for testing.
             .allowMainThreadQueries()
@@ -99,12 +98,12 @@ class ItemDaoTest {
     @Throws(Exception::class)
     fun daoUpdateItems_updatesItemsInDB() = runBlocking {
         addTwoItemsToDb()
-        itemDao.update(Item(1, "Apples", 15.0, 25))
-        itemDao.update(Item(2, "Bananas", 5.0, 50))
+        itemDao.update(Item(1, "Manzanas", 15.0, 25))
+        itemDao.update(Item(2, "Platanos", 5.0, 50))
 
         val allItems = itemDao.getAllItems().first()
-        assertEquals(allItems[0], Item(1, "Apples", 15.0, 25))
-        assertEquals(allItems[1], Item(2, "Bananas", 5.0, 50))
+        assertEquals(allItems[0], Item(1, "Manzanas", 15.0, 25))
+        assertEquals(allItems[1], Item(2, "Platanos", 5.0, 50))
     }
 
     private suspend fun addOneItemToDb() {
